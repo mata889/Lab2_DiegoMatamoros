fcb74a6713158d3d048120ebebdd71143d6fd752
mata889 <dam_900@unitec.edu>
2017-07-28 14:49:21 -0600

    se ha creado un menu principal

--- src/lab2_diegomatamoros/Lab2_DiegoMatamoros.java
+++ src/lab2_diegomatamoros/Lab2_DiegoMatamoros.java
@@ -1,12 +1,59 @@
-
 package lab2_diegomatamoros;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class Lab2_DiegoMatamoros {
 
+    static String Nombre;
+    static String Edad;
+    static String Lugar;
+    static String User;
+    static String lista;
+    static String pass;
+    static String listaS;
+    static String listaM;
+    static String buzon;
+    static String menu;
+    static ArrayList Usuario = new ArrayList();
+
     public static void main(String[] args) {
-        
-        
+        System.out.println("-------MiniRed------");
+        Usuario u = new Usuario();
+        do {
+
+            menu = JOptionPane.showInputDialog("1.Crear usuario\n 2.Modificar datos \n 3.Eliminar Usuario \n 4.Un Login \n 0. SALIR");
+            switch (menu) {
+                case "1":
+                    Nombre = JOptionPane.showInputDialog("Introduzca su nombre");
+                    Edad = JOptionPane.showInputDialog("Escriba Edad");
+                    Lugar = JOptionPane.showInputDialog("Ingresar Lugar");
+                    User = JOptionPane.showInputDialog("Introduzca user");
+                    pass = JOptionPane.showInputDialog("SU passwoord");
+                    JOptionPane.showMessageDialog(null, "Ha creado un usuario nuevo");
+                    Usuario.add(new Usuario(Nombre, Edad, Lugar, User, pass));
+                    String s = "";
+                    int cont=0;
+                    for (Object us : Usuario) {
+                        
+                        if (us instanceof Usuario) {
+                            s+= ""+u.getNombre().indexOf(cont)+"\n";
+                        }
+                        cont++;
+                    }
+                    JOptionPane.showMessageDialog(null, s);
+                    break;
+                case "2":
+                    JOptionPane.showMessageDialog(null, "Ha entrado al menu de modificacion");
+
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+            }
+        } while (!"0".equals(menu));
+
     }
-    
+
 }
---
+++ src/lab2_diegomatamoros/Usuario.java
@@ -0,0 +1,103 @@
+package lab2_diegomatamoros;
+
+import java.util.ArrayList;
+
+public class Usuario {
+
+    public String Nombre;
+    public String Edad;
+    public String Lugar;
+    public String User;
+    public String Pass;
+
+    ArrayList Listadeamigos = new ArrayList();
+    ArrayList ListaDeMen = new ArrayList();
+    ArrayList ListaDeSoli = new ArrayList();
+    ArrayList Buzon = new ArrayList();
+
+    public Usuario() {
+    }
+
+    public Usuario(String Nombre,String Edad,String Lugar,String User,String Pass) {
+        this.Nombre = Nombre;
+        this.Edad=Edad;
+        this.Lugar=Lugar;
+        this.User=User;
+        this.Pass=Pass;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public String getLugar() {
+        return Lugar;
+    }
+
+    public void setLugar(String Lugar) {
+        this.Lugar = Lugar;
+    }
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String User) {
+        this.User = User;
+    }
+
+    public String getPass() {
+        return Pass;
+    }
+
+    public void setPass(String Pass) {
+        this.Pass = Pass;
+    }
+
+    public ArrayList getListadeamigos() {
+        return Listadeamigos;
+    }
+
+    public void setListadeamigos(ArrayList Listadeamigos) {
+        this.Listadeamigos = Listadeamigos;
+    }
+
+    public ArrayList getListaDeMen() {
+        return ListaDeMen;
+    }
+
+    public void setListaDeMen(ArrayList ListaDeMen) {
+        this.ListaDeMen = ListaDeMen;
+    }
+
+    public ArrayList getListaDeSoli() {
+        return ListaDeSoli;
+    }
+
+    public void setListaDeSoli(ArrayList ListaDeSoli) {
+        this.ListaDeSoli = ListaDeSoli;
+    }
+
+    public ArrayList getBuzon() {
+        return Buzon;
+    }
+
+    public void setBuzon(ArrayList Buzon) {
+        this.Buzon = Buzon;
+    }
+
+    
+
+}
